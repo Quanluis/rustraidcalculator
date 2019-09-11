9af8a4097ad72b28c1f034b290d3c748951d9617
Quanluis <quanluis@protonmail.com>
2019-09-11 17:33:52 -0400

    completed program, could add more in the future.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,37 @@ using namespace std;
 
 int main(){
 
-    cout << "Hello Rust";
+    //variables
+
+    int wooden,
+        sheetMetal,
+        woodenBreak,
+        sheetMetalBreak,
+        armored,
+        armoredBreak,
+        finalResults;
+
+    cout << "Wooden? " << endl;
+
+    cin >> wooden;
+
+        woodenBreak = wooden * 2;
+
+    cout << "SheetMetal? " << endl;
+
+        cin >> sheetMetal;
+
+        sheetMetalBreak = sheetMetal * 4;
+
+    cout << "Armored?" << endl;
+
+        cin >> armored;
+
+        armoredBreak = armored * 12;
+
+        finalResults = woodenBreak + sheetMetalBreak + armoredBreak;
+
+        cout << "Players will need " << finalResults << " satchels to raid base.";
 
     return 0;
 }
